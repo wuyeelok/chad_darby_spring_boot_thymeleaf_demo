e1a188c4ae20ef234a27a051d12fb54fe326ae30
wuyeelok <wuyeelok@gmail.com>
2020-03-31 22:01:43 +0800

    Config: Setup JPA and MySQL employee DB and table
    For detail, see function: CommandLineRunner dbDataInit

--- src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
@@ -1,10 +1,28 @@
-package com.luv2code.springboot.thymeleafdemo.model;
+package com.luv2code.springboot.thymeleafdemo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
+
+	@Column(name = "first_name")
 	private String firstName;
+
+	@Column(name = "last_name")
 	private String lastName;
+
+	@Column(name = "email")
 	private String email;
 
 	public Employee() {
@@ -13,6 +31,12 @@ public class Employee {
 
 	public Employee(int id, String firstName, String lastName, String email) {
 		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public Employee(String firstName, String lastName, String email) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
