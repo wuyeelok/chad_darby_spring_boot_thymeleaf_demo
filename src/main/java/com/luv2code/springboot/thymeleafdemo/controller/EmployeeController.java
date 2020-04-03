b9ae2032aabbf02481c1330e1cc1aef763a5595a
wuyeelok <wuyeelok@gmail.com>
2020-04-03 18:27:17 +0800

    List: Get data from DB instead of static

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,48 +1,35 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	// Load employee data
+	private EmployeeService employeeService;
 
-	private List<Employee> theEmployees;
-
-	@PostConstruct
-	private void loadData() {
-
-		// Create employee
-		Employee emp1 = new Employee(1, "JJ", "AAA", "jjaa@gmail.com");
-		Employee emp2 = new Employee(2, "TTT", "XXXX", "tx@gmail.com");
-		Employee emp3 = new Employee(3, "WW", "CCC", "wc@gmail.com");
-
-		// Create the list
-		this.theEmployees = new ArrayList<>();
-
-		// Add to the list
-		this.theEmployees.add(emp1);
-		this.theEmployees.add(emp2);
-		this.theEmployees.add(emp3);
+	@Autowired
+	public EmployeeController(EmployeeService theEmployeeService) {
+		this.employeeService = theEmployeeService;
 	}
 
 	// Add mapping for "/list"
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
 
+		List<Employee> theEmployees = this.employeeService.findAll();
+
 		// Add to the spring model
-		theModel.addAttribute("employees", this.theEmployees);
+		theModel.addAttribute("employees", theEmployees);
 
 		return "list-employees";
 	}
