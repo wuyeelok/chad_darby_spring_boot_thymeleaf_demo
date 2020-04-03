dc8c295007ef8c7b7d763296f2aaaf0e3e932072
wuyeelok <wuyeelok@gmail.com>
2020-04-03 18:13:35 +0800

    Service: Copy service from previous

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,54 @@
+package com.luv2code.springboot.thymeleafdemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luv2code.springboot.thymeleafdemo.dao.EmployeeRepository;
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeRepository employeeRepository;
+
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+		this.employeeRepository = theEmployeeRepository;
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		// return employeeRepository.findAllByOrderByLastNameAsc();
+		return this.employeeRepository.findAll();
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		Optional<Employee> result = this.employeeRepository.findById(theId);
+
+		Employee theEmployee = null;
+
+		if (result.isPresent()) {
+			theEmployee = result.get();
+		} else {
+			// we didn't find the employee
+			throw new RuntimeException("Did not find employee id - " + theId);
+		}
+
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		this.employeeRepository.save(theEmployee);
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		this.employeeRepository.deleteById(theId);
+	}
+
+}
